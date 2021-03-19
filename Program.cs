ce85d4ff7278769062fc4b592c707c730e2e6788
Yoon-JH-0 <80893679+Yoon-JH-0@users.noreply.github.com>
2021-03-19 14:44:25 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Middle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scores MyScores = new Scores(1);
+
+            MyScores.Input();
+            MyScores.Sort(1);
+            MyScores.Output();
+        }
+    }
+}
